3f8501b3c7f9d0781c34521b98fbe7cdeee4ebac
ErikSalva <erikshalva@gmail.com>
2024-07-28 21:25:31 -0300

    Estructura basica del proyecto, instale react y chakra

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+import Header from './Components/Header/Header'
+import Main from './Components/Main/Main'
+import Footer from './Components/Footer/Footer'
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import './Header.css'
+
+import {
+    Box,
+    Flex,
+    Avatar,
+    HStack,
+    Text,
+    IconButton,
+    Button,
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    MenuDivider,
+    useDisclosure,
+    useColorModeValue,
+    Stack,
+  } from '@chakra-ui/react'
+  import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons'
+
+
+const Header = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+
+    return (
+        <>
+            <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4} as='header'>
+                <p>hola</p>
+                <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+                    <IconButton
+                        size={'md'}
+                        icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+                        aria-label={'Open Menu'}
+                        display={{ md: 'none' }}
+                        onClick={isOpen ? onClose : onOpen}
+                    />
+
+                </Flex>
+
+            </Box>
+           
+        </>
+    );
+}
+
+export default Header;
