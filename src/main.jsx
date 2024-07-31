097179f67b36e9a6bd90879924b2563896525ed0
ErikSalva <erikshalva@gmail.com>
2024-07-31 01:13:45 -0300

    Repare un error en main

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import App from './App.jsx'
 import './index.css'
 import {ChakraProvider} from '@chakra-ui/react'
 
-import { linktheme } from './theme/link.js'
 import {theme} from './theme/theme.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
